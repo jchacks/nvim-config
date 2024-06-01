858128547cb23f1385acd15ae23b9af345152c62
Joshua Chacksfield <chacksfieldj@gmail.com>
2024-06-01 15:15:48 +0200

    Added bufferline

---
+++ lua/plugins/editor.lua
@@ -0,0 +1,24 @@
+return {
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+		},
+		config = function()
+			vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle left<CR>", {})
+		end,
+	},
+	{
+		"akinsho/bufferline.nvim",
+		version = "*",
+		dependencies = "nvim-tree/nvim-web-devicons",
+		config = function()
+      vim.opt.termguicolors = true
+      require("bufferline").setup({})
+		end,
+	},
+}
