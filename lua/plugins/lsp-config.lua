c7b97815d86ce36cda10bb6f3e787061d99cef0e
Joshua Chacksfield <chacksfieldj@gmail.com>
2024-06-02 17:39:57 +0200

    Cleaned up lsp config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,103 +1,105 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "ruff",
-          "basedpyright",
-          "rust_analyzer",
-        },
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"j-hui/fidget.nvim",
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+		},
+		config = function()
+			require("fidget").setup() -- ls status
+			require("mason").setup()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"ruff",
+					"basedpyright",
+					"rust_analyzer",
+				},
+			})
 
-      -- lua
-      lspconfig.lua_ls.setup({})
-      -- rust
-      lspconfig.rust_analyzer.setup({})
-      -- python
-      lspconfig.basedpyright.setup({
-        filetypes = { "python" },
-        basedpyright = {
-          -- Using Ruff's import organizer
-          disableOrganizeImports = true,
-          analysis = {
-            -- Ignore all files for analysis to exclusively use Ruff for linting
-            ignore = { "*" },
-          },
-        },
-        python = {
-          venvPath = ".",
-          venv = ".venv",
-        },
-      })
+			local lspconfig = require("lspconfig")
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
 
-      lspconfig.ruff.setup({
-        cmd = { "ruff", "server", "--preview" },
-        filetypes = { "python" },
-      })
-    end,
-  },
-  {
-    -- wrap non-ls into ls
-    "nvimtools/none-ls.nvim",
-    config = function()
-      local null_ls = require("null-ls")
-      null_ls.setup({
-        sources = {
-          null_ls.builtins.formatting.stylua,
-          null_ls.builtins.diagnostics.mypy,
-        },
-      })
-    end,
-  },
-  {
-    -- completions
-    "hrsh7th/nvim-cmp",
-    config = function()
-      local cmp = require("cmp")
+			-- lua
+			lspconfig.lua_ls.setup({})
+			-- rust
+			lspconfig.rust_analyzer.setup({})
+			-- python
+			lspconfig.basedpyright.setup({
+				filetypes = { "python" },
+				basedpyright = {
+					-- Using Ruff's import organizer
+					disableOrganizeImports = true,
+					analysis = {
+						-- Ignore all files for analysis to exclusively use Ruff for linting
+						ignore = { "*" },
+					},
+				},
+				python = {
+					venvPath = ".",
+					venv = ".venv",
+				},
+			})
 
-      cmp.setup({
-        snippet = {
-          -- REQUIRED - you must specify a snippet engine
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        },
-        window = {
-          -- completion = cmp.config.window.bordered(),
-          -- documentation = cmp.config.window.bordered(),
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        }),
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-        }, {
-          { name = "buffer" },
-        }),
-      })
-    end,
-  },
+			lspconfig.ruff.setup({
+				cmd = { "ruff", "server", "--preview" },
+				filetypes = { "python" },
+			})
+		end,
+	},
+	{
+		-- wrap non-ls into ls
+		"nvimtools/none-ls.nvim",
+		config = function()
+			local null_ls = require("null-ls")
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.stylua,
+					null_ls.builtins.diagnostics.mypy,
+				},
+			})
+		end,
+	},
+	{
+		-- completions
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+		},
+		config = function()
+			local cmp = require("cmp")
+
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						vim.snippet.expand(args.body)
+					end,
+				},
+				window = {
+					-- completion = cmp.config.window.bordered(),
+					-- documentation = cmp.config.window.bordered(),
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					-- { name = "luasnip" },
+				}, {
+					{ name = "buffer" },
+				}),
+			})
+		end,
+	},
 }
